4d57dc0fb9df40ae0728af311b50ace034fdaa6c
stephanRol <s.roloff01@gmail.com>
2021-05-02 20:30:19 +0200

    Layout finished

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const menuToggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+})
